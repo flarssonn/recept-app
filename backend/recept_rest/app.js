82d9695ee37f54257afd7322999e8123f619b5c8
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-02-27 17:14:55 +0100

    try to fix cors stuff

--- backend/recept_rest/app.js
+++ backend/recept_rest/app.js
@@ -10,6 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+// CORS enable (if request from other domains)
+app.use('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -42,5 +50,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- frontend/recept-react-app/src/components/header.js
+++ frontend/recept-react-app/src/components/header.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './frying-pan.svg';
-import backgroundVeg from './veg.jpg';
+
 //import './App.css';
 import './header.css'
 
