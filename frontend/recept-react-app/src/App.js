7f7bd02a13485150bd14fd2b107c35e8dfce8299
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-02-27 08:31:28 +0100

    fixed components

--- frontend/recept-react-app/src/App.js
+++ frontend/recept-react-app/src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
-import logo from './frying-pan.svg';
-import backgroundVeg from './veg.jpg';
+import ReactDOM from 'react-dom';
+//import logo from './frying-pan.svg';
+//import backgroundVeg from './veg.jpg';
 import './App.css';
+import {Header} from './components/header.js'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <h1 className="Super-header">The Recipe App</h1>
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h3>Find the recipe for your needs</h3>
-                </div>
-                <div className="App-navbar">
-
-                </div>
+            <div>
+                <Header />
             </div>
         );
     }
 }
+
 
 export default App;
