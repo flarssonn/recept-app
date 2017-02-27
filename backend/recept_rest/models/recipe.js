4257a4afbe38cf37c6f8f6b876251bc23b6dbdc8
William Dahlberg <dahlberg.william@gmail.com>
2017-02-27 15:06:18 +0100

    removed some uneccessary

--- backend/recept_rest/models/recipe.js
+++ backend/recept_rest/models/recipe.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         // associations can be defined here
         Recipe.belongsTo(models.User);
-        //Recipe.belongsToMany(models.Tag);
+        Recipe.belongsToMany(models.Tag, {through: models.IsTagged});
       }
     }
   });
