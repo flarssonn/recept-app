277a7b548e43d6d98ae1c43d57c251f072352a7d
William Dahlberg <dahlberg.william@gmail.com>
2017-02-27 14:14:04 +0100

    index route test

--- backend/recept_rest/routes/index.js
+++ backend/recept_rest/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'nej' });
+  res.render({ title: 'nej' });
 });
 
 module.exports = router;
