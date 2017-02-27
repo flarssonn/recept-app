5720b0a0c40035d5d09cb92071a00028afc42d74
William Dahlberg <mail@golvmopp.it>
2017-02-27 11:39:47 +0100

    dadbauybaneofaef

---
+++ backend/recept_rest/models/recipe.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Recipe = sequelize.define('Recipe', {
+    title: DataTypes.STRING,
+    ingredients: DataTypes.TEXT,
+    description: DataTypes.TEXT,
+    piclink: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        Recipe.belongsTo(models.User);
+        //Recipe.belongsToMany(models.Tag);
+      }
+    }
+  });
+  return Recipe;
+};
