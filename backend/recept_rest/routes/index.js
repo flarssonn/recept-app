181caff285ba899e902aa4e1e5b38befeed3e409
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-03-01 10:42:03 +0100

    Successful AJAX

--- backend/recept_rest/routes/index.js
+++ backend/recept_rest/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.json("asd");
   res.render('index', { title: 'nej' });
+
 });
 
 module.exports = router;
