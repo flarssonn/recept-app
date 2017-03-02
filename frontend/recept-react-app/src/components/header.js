196330314eba50e68c084c2437371af4c56ee2bc
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-03-02 09:13:24 +0100

    Added footer

--- frontend/recept-react-app/src/components/header.js
+++ frontend/recept-react-app/src/components/header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import logo from './frying-pan.svg';
-
-//import './App.css';
-import './header.css'
+import './header.css';
+import {Navbar} from './navbar.js';
 
 class Header extends React.Component  {
 
     render() {
         return(
-            <div className="header">
-                <h1 className="Super-header">The Recipe App</h1>
-                <img className="App-logo" src={logo}  alt="logo" />
-                <h3>Find the recipe for your needs</h3>
+            <div>
+                <div className="header">
+                    <h1 className="Super-header">The Recipe App</h1>
+                    <img className="App-logo" src={logo}  alt="logo" />
+                    <h3>Find the recipe for your needs</h3>
+                </div>
+                <Navbar />
             </div>
         );
 
---
+++ frontend/recept-react-app/src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+
+class Navbar extends React.Component  {
+
+    render() {
+        return(
+
+            <div className="navbar">
+                <h1 className="h1-footer">YOOOOOO</h1>
+            </div>
+
+        );
+    }
+
+}
+
+export {Navbar};
