cdca59f7c4b23939259aa10d59193cac792c4aae
VOB <vobc@live.com>
2017-03-02 15:05:36 +0100

    recipe router

---
+++ backend/recept_rest/routes/recipes.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var Recipe = require('../models/recipe.js');
+
+//Get all recipes
+router.get('/', function(req, res, next) {
+    // Promised based
+    Recipe.findAll().then(function(recipes) {
+        res.json(recipes);
+    });
+});
+
+//Get single recipe
+router.get('/:id', function(req, res, next) {
+    // Promised based
+    Recipe.findById(id).then(function(recipe) {
+        res.json(recipe);
+    });
+});
+
+//Add a recipe
+router.post('/add', function(req, res, next) {
+    Recipe.create({
+      title: title,
+      ingredients: ingredients,
+      description: description,
+      piclink: piclink
+    }).then(function() {
+      res.redirect('/');
+    });
+});
+
+
+//Delete a recipe
+router.post('/delete/:id', function(req, res, next) {
+    var rid = parseInt(req.params.id);
+    Recipe.destroy({
+      where: {
+        id: rid
+      };
+});
+module.exports = router;
