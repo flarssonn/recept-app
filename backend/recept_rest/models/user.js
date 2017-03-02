18998468f464da141c4779bcf1fc1878852ac7bb
William Dahlberg <dahlberg.william@gmail.com>
2017-03-02 16:09:58 +0100

    test setup and assosiactionO dfsdjf for databse

--- backend/recept_rest/models/user.js
+++ backend/recept_rest/models/user.js
@@ -8,7 +8,9 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        User.hasMany(models.Recipe);
+        User.hasMany(models.Recipe, {
+          as: "recipes"
+        });
       }
     }
   });
