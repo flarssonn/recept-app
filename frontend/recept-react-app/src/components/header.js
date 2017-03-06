74d804046b826f9fe6be7dfa6cdf86d4d4796368
Aringskog <a.aringskog@gmail.com>
2017-03-06 18:48:17 +0100

    some buttons

--- frontend/recept-react-app/src/components/header.js
+++ frontend/recept-react-app/src/components/header.js
@@ -11,7 +11,12 @@ class Header extends React.Component  {
             <div className="header">
                 <h1 className="Super-header">The Recipe App</h1>
                 <img className="App-logo" src={logo}  alt="logo" />
-                <h3>Find the recipe for your needs</h3>
+                <h3>Find the recipie for your needs</h3>
+                <div className="btn-group" role="group" aria-label="Header buttons">
+                  <button type="button" className="btn btn-secondary">Fav</button>
+                  <button type="button" className="btn btn-secondary">Prof</button>
+                  <button type="button" className="btn btn-secondary">Pref</button>
+                </div>
             </div>
         );
 
