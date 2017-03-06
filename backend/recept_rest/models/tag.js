271c3d50a2b17a78fc09641b0cb9160a1a4e0dea
William Dahlberg <mail@golvmopp.it>
2017-03-06 12:16:26 +0100

    missing curly bracket

--- backend/recept_rest/models/tag.js
+++ backend/recept_rest/models/tag.js
@@ -4,6 +4,7 @@ module.exports = function(sequelize, DataTypes) {
     tag: {
       type: DataTypes.STRING,
       unique: true
+    }
   }, {
     classMethods: {
       associate: function(models) {
