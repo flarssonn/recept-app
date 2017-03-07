15a646f1edb3c8b1022e64242d836c285c0878d9
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-03-07 16:45:21 +0100

    Started on footer

--- frontend/recept-react-app/src/components/footer.js
+++ frontend/recept-react-app/src/components/footer.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './header.css';
+import {Button} from 'react-bootstrap';
+import {ButtonToolbar} from 'react-bootstrap'
+import {Grid} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+
 
 class Footer extends React.Component  {
 
@@ -7,7 +13,20 @@ class Footer extends React.Component  {
         return(
 
             <div className="footer">
-                <h1 className="h1-footer">YOOOOOO</h1>
+                <Grid>
+                    <Row className="show-grid">
+                        <Col xs={12} md={8}>
+                            <div className="Creator-label">
+                                This site was created by
+                            </div>
+                        </Col>
+                        <Col xs={6} md={4}>
+                            <div className="Creator-pics">
+                                
+                            </div>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
 
         );
