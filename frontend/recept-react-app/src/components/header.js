4958d01f2c734d1c0ee72a5500a0a843031ae00d
Aringskog <a.aringskog@gmail.com>
2017-03-07 14:14:01 +0100

    trying to make some grids

--- frontend/recept-react-app/src/components/header.js
+++ frontend/recept-react-app/src/components/header.js
@@ -4,39 +4,52 @@ import './header.css';
 import {Navbar} from './navbar.js';
 import { Button } from 'react-bootstrap';
 import { ButtonToolbar} from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 
 class Header extends React.Component  {
 
     render() {
         return(
           <div>
-            <div className="header">
-                <h1 className="Super-header">The Recipe App</h1>
-                <img className="App-logo" src={logo}  alt="logo" />
-                <h3>Find the recipie for your needs</h3>
+            <Grid>
+              <Row className="show-grid">
+                <Col xs={12} md={8}>
+                    <div className="header">
+                        <h1 className="Super-header">The Recipe App</h1>
+                        <img className="App-logo" src={logo}  alt="logo" />
+                        <h3>Find the recipie for your needs</h3>
+                    </div>
+                </Col>
+                <Col>
+                <h1>Hej</h1>
 
+                </Col>
 
-                <ButtonToolbar >
-                  <Button>Default</Button>
+                <div>
+                  <ButtonToolbar >
+                    <Button>Default</Button>
 
-                  <Button bsStyle="primary">Primary</Button>
+                    <Button bsStyle="primary">Primary</Button>
 
-                  <Button bsStyle="success">Success</Button>
+                    <Button bsStyle="success">Success</Button>
 
-                  <Button bsStyle="info">Info</Button>
+                    <Button bsStyle="info">Info</Button>
 
-                  <Button bsStyle="warning">Warning</Button>
+                    <Button bsStyle="warning">Warning</Button>
 
-                  <Button bsStyle="danger">Danger</Button>
+                    <Button bsStyle="danger">Danger</Button>
 
-                  <Button bsStyle="link">Link</Button>
-                </ButtonToolbar>
+                    <Button bsStyle="link">Link</Button>
+                    </ButtonToolbar>
 
-                </div>
-
-            <div>
-                <Navbar />
-            </div>
+              </div>
+              </Row>
+              <div>
+                  <Navbar />
+              </div>
+            </Grid>
           </div>
       );
 
