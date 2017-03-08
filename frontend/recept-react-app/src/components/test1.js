7d9ea1a5d5ab6820c43060e59f2cb039f0384070
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-03-08 11:46:46 +0100

    First router

--- frontend/recept-react-app/src/components/test1.js
+++ frontend/recept-react-app/src/components/test1.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './header.css';
+import { Link } from 'react-router'
+
 
 class Test1 extends React.Component  {
 
     render() {
         return(
           <div>
-            <h1>Axel</h1>
+            <Link to="/">Hejsan</Link>
           </div>
       );
 
