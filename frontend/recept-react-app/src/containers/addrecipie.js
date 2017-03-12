525b8c0088bf28bd19664f0448a63aa80abfc631
Filip Larsson <filip.larsson.vbg@gmail.com>
2017-03-12 22:25:36 +0100

    adding recipe now goes into the database

--- frontend/recept-react-app/src/containers/addrecipie.js
+++ frontend/recept-react-app/src/containers/addrecipie.js
@@ -2,6 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addRecipie } from '../actions'
 import { Button } from 'react-bootstrap';
+import $ from 'jquery'
+
+function add(recipeTitle){
+  var addUrl = 'http://localhost:3001/recipes/add/b@a'
+  var data = {
+    title: recipeTitle,
+    ingredients: "tomato",
+    description: "cook it",
+    piclink: "",
+    tags: "Axel, Aringskog"
+  };
+  $.ajax({
+      url: addUrl,
+      dataType: 'json',
+      cache: false,
+      type: 'POST',
+      data: data,
+      success: function(data) {
+          console.log('Success');
+      },
+      error: function(err) {
+          alert('Something went wrong: ' + err);
+      }
+  });
+}
 
 let AddRecipie = ({ dispatch }) => {
   let input
@@ -13,6 +38,7 @@ let AddRecipie = ({ dispatch }) => {
         if (!input.value.trim()) {
           return
         }
+        add(input.value)
         dispatch(addRecipie(input.value))
         input.value = ''
       }}>
