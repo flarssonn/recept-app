3aa8df6062b44196b772ee90a4bd70eeb6264066
VOB <vobc@live.com>
2017-04-10 12:16:46 +0200

    changed favorites to depend on username instead of id

--- backend/recept_rest/routes/favourites.js
+++ backend/recept_rest/routes/favourites.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var models = require('../models');
 
 // Gets all favourites belonging to a user
-router.get('/:uid', function(req, res, next) {
-  var uid = req.params.uid;
-  models.User.findById(uid).then(function(user) {
+router.get('/:username', function(req, res, next) {
+  var username = req.params.username;
+  models.User.findOne({
+    where: {username: username}
+  }).then(function(user) {
     user.getFavourites().then(function(favs) {
       res.json(favs);
     });
@@ -13,10 +15,12 @@ router.get('/:uid', function(req, res, next) {
 });
 
 // Add a favourite to a user
-router.post('/add/:uid/:rid', function(req, res, next) {
-  var uid = req.params.uid;
+router.post('/add/:username/:rid', function(req, res, next) {
+  var username = req.params.username;
   var rid = req.params.rid;
-  models.User.findById(uid).then(function(user) {
+  models.User.findOne({
+    where: {username: username}
+  }).then(function(user) {
     models.Recipe.findById(rid).then(function(recipe) {
       user.addFavourite(recipe).then(function() {
         res.json("fav added");
@@ -26,8 +30,8 @@ router.post('/add/:uid/:rid', function(req, res, next) {
 });
 
 // Delete a favourite from a user
-router.get('/delete/:uid/:rid', function(req, res, next) {
-  var uid = req.params.uid;
+router.get('/delete/:username/:rid', function(req, res, next) {
+  var username = req.params.username;
   var rid = req.params.rid;
 });
 
